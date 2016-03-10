a1cf497ef52aa70e8e1cca554dfac8d41481912e
nick2383 <nicholas.birch2@gmail.com>
2016-03-10 15:28:51 -0800

    actually finished problem 1

--- problem_1.rb
+++ problem_1.rb
@@ -2,8 +2,8 @@
 # Nick Birch
 sum = 0
 
-(1..10000).to_a.each{|element| 
-	if (element%3 == 0 || element%5 ==0)
+(1..999).to_a.each{|element| 
+	if (element%3 == 0 || element%5 == 0)
 		sum = sum + element
 	end
 }
